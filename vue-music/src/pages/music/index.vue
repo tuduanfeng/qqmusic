0c69875d86e6076168181f034fd899115f1bf858
tudanfeng <1490649846@qq.com>
2018-04-24 11:24:55 +0800

    music

---
+++ vue-music/src/pages/music/index.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="m-header">
+        <h1 class="m-header-text">QQ Music</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            
+        }
+    },
+    created(){
+//      this.axios({
+//         method:"get",
+//         url:'http://music.qq.com/musicbox/shop/v3/data/hit/hit_newsong.js',
+//         headers:{
+//          contentType:"jsonp"
+//         }
+//      })
+        
+     
+        this.axios.get('/api/musicbox/shop/v3/data/hit/hit_newsong.js')
+        .then(res=>{
+            console.log(res)
+        }).catch(res=>{
+            console.log(res)
+        });
+    
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style>
+    body{
+        background: #222;
+    }
+    .m-header .m-header-text{
+        text-align: center;
+        color: #ffcd32;
+        line-height: .8rem;
+        font-size: .25rem;
+    }
+
+</style>
